305de922eefb6eb0d6b3ed6601395678095d6055
mbax9an4 <alina.neculae@manchester.postgrad.ac.uk>
2020-09-18 19:37:59 +0100

    implemented a new version of eval_KDE_matrix to speed up operations

--- KDE.py
+++ KDE.py
@@ -7,18 +7,17 @@ import matplotlib.pyplot as plt
 
 # @profile
 # compute the mean log probability over the test data
-def eval_KDE(train_set, test_set, best_sigma):
+def eval_KDE(train_set, test_set, sigma):
     # compute the constant terms of the likelihood 
-    denom = -1*2.0*math.pow(best_sigma,2)
-    norm = -1.0/2 * math.log(2.0*math.pi*math.pow(best_sigma,2))
+    denom = -1*2.0*math.pow(sigma,2)
+    norm = -1.0/2 * math.log(2.0*math.pi*math.pow(sigma,2))
     prior = math.log(1.0/train_set.shape[0])
-    shift_constant = 0
 
-    # 
+    # variable to record the mean log prob
     mean_log_prob = 0.0
     
     # for each example in test compute the likelihood - average 
-    for test_ex in tqdm(test_set):
+    for test_ex in tqdm(test_set, desc=f"Sigma: {sigma}"):
     # for test_ex in test_set:
         # import pdb; pdb.set_trace()
     
@@ -31,11 +30,13 @@ def eval_KDE(train_set, test_set, best_sigma):
         # add the probabilities over the features
         cond_prob_features = np.sum(cond_prob_features_norm, axis=1)
 
+        # check that we have reduced the right dimension
         assert cond_prob_features.shape == (train_set.shape[0], )
 
         # add the prior
         cond_prob_features = cond_prob_features + prior
 
+        # to reduce the risk of overflows we perform the following operations using a log-sum-exp numerical trick 
         shift_constant = np.max(cond_prob_features)
 
         # raise the values to exp, while shifting the values to ensure no overflow occurs
@@ -47,47 +48,52 @@ def eval_KDE(train_set, test_set, best_sigma):
 
     return mean_log_prob/test_set.shape[0]
 
-def eval_KDE_matrix(train_set, test_set, best_sigma):
+# @profile
+def eval_KDE_matrix(train_set, test_set, sigma, batch_size):
     # compute the constant terms of the likelihood 
-    denom = -1*2.0*math.pow(best_sigma,2)
-    norm = -1.0/2 * math.log(2.0*math.pi*math.pow(best_sigma,2))
+    denom = -1*2.0*math.pow(sigma,2)
+    norm = -1.0/2 * math.log(2.0*math.pi*math.pow(sigma,2))
     prior = math.log(1.0/train_set.shape[0])
-    shift_constant = 0
 
     # variable to record the mean log probability over the given dataset
     mean_log_prob = 0.0
 
-    # import pdb; pdb.set_trace()
+    for b in range(0, test_set.shape[0], batch_size):
+        # import pdb; pdb.set_trace()
 
-    # compute the distance between each test example and all train examples 
-    diff_matrix = np.array([np.square(test_ex - train_set) for test_ex in test_set])
+        # compute the distance between each test example and all train examples 
+        diff_matrix = np.square(test_ex[:, np.newaxis] - train_set)
 
-    print(diff_matrix.shape)
+        # divide the distances by the denominator
+        cond_prob_features_norm = np.true_divide(diff_matrix, denom) + norm
 
-    # divide the distances by the denominator
-    cond_prob_features_norm = np.true_divide(diff_matrix, denom) + norm
+        # add the probabilities over the features
+        cond_prob_features = np.sum(cond_prob_features_norm, axis=2)
 
-    # add the probabilities over the features
-    cond_prob_features = np.sum(cond_prob_features_norm, axis=2)
+        # check that we have reduced over the right dimension
+        assert cond_prob_features.shape == (batch_size, train_set.shape[0], )
 
-    assert cond_prob_features.shape == (test_set.shape[0], train_set.shape[0], )
+        # add the prior
+        cond_prob_features = cond_prob_features + prior
 
-    # add the prior
-    cond_prob_features = cond_prob_features + prior
+        # find the maximum value at each test example
+        shift_constant = np.array([np.ones(train_set.shape[0])*np.max(e) for e in cond_prob_features])
 
-    # find the maximum value at each test example
-    shift_constant = [np.max(e) for e in cond_prob_features]
+        # raise the values to exp, while shifting the values to ensure no overflow occurs
+        likelihoods = np.exp(cond_prob_features - shift_constant)
 
-    # raise the values to exp, while shifting the values to ensure no overflow occurs
-    likelihoods = np.exp(cond_prob_features - shift_constant)
+        log_probs = np.add(np.log(np.sum(likelihoods, axis=1)), shift_constant[:,0])
 
-    log_probs = math.log(np.sum(likelihoods, axis=1)) + shift_constant
+        mean_log_prob += np.sum(log_probs)
 
-    return np.mean(log_probs)
+    return mean_log_prob/test_set.shape[0]
 
 
 # fit the best sigma value
 def fit_sigma(train_set, valid_set):
+    # sigma values over which we grid search
+    config['sigma_grid'] = [0.01, 0.03, 0.05, 0.08, 0.1, 0.2, 0.5, 0.8, 1.0, 1.2, 1.5, 1.7, 2.0, 2.3]
+
     mean_log_prob = []
     for sigma in config['sigma_grid']:
         mean_log_prob.append(eval_KDE(train_set, valid_set, sigma))
@@ -96,7 +102,7 @@ def fit_sigma(train_set, valid_set):
     plt.plot(config['sigma_grid'], mean_log_prob)
     plt.savefig(f"{config['dset_name']}_sigma_grid.pdf", format='pdf')
 
-    return sorted(zip(config['sigma_grid'], mean_log_prob), key=lambda x: x[1])[-1]
+    return sorted(zip(config['sigma_grid'], mean_log_prob), key=lambda x: x[1])[-1][0]
 
 if __name__ == "__main__":
     with open("config.json", "r") as f:
@@ -107,17 +113,20 @@ if __name__ == "__main__":
     train_set, valid_set, test_set = load_mnist(config)
 
     # best_sigma = fit_sigma(train_set, valid_set)
-    best_sigma = 0.25
-
+    best_sigma = 0.5
+    
     mean_log_prob = eval_KDE(train_set, test_set, best_sigma)
     print(mean_log_prob)
+
+    # mean_log_prob = eval_KDE_matrix(train_set, test_set, best_sigma, 5)
+    # print(mean_log_prob)
 
     # load the cifar100 data
     config['dset_name'] = "cifar"
     train_set, valid_set, test_set = load_cifar(config)
 
-    # best_sigma = fit_sigma(train_set, valid_set)
-    best_sigma = 0.25
+    best_sigma = fit_sigma(train_set, valid_set)
+    # best_sigma = 0.25
 
     mean_log_prob = eval_KDE(train_set, test_set, best_sigma)
     print(mean_log_prob)
--- tests.py
+++ tests.py
@@ -3,13 +3,14 @@ from preproc_data import load_mnist, load_cifar
 import numpy as np
 import random
 import json
+import time
 from sklearn.neighbors import KernelDensity
 
 def validate_result():
     with open("config.json", "r") as f:
         config = json.load(f)
 
-    for r in [2, 10, 30]:
+    for r in [2, 10, 20]:
         rand_seed = r
         config['rand_seed'] = r
 
@@ -20,39 +21,51 @@ def validate_result():
         config['dset_name'] = "mnist"
         # load the mnist data
         train_set, valid_set, test_set = load_mnist(config)
-        test_set = test_set[:3000]
+        test_set = test_set[:2000]
 
+        start = time.time()
         KDE_sklearn = KernelDensity(bandwidth=1)
         KDE_sklearn.fit(train_set)
         log_sklearn = KDE_sklearn.score(test_set)
-        print(f'with a random seed of {r} the sklearn mean log prob on mnist is {log_sklearn/test_set.shape[0]}')
+        end = time.time()
+        print(f'with a random seed of {r} the sklearn mean log prob on mnist is {log_sklearn/test_set.shape[0]} and it took {end-start}s')
         # print('sklearn mean log prob -740.8322056334938')
 
         best_sigma = 1
+        start = time.time()
         mean_log_prob = eval_KDE(train_set, test_set, best_sigma)
-        print(f'with a random seed of {r} my mean log prob on mnist is {mean_log_prob}')
+        end = time.time()
+        print(f'with a random seed of {r} my mean log prob on mnist is {mean_log_prob} and it took {end-start}s')
 
-        mean_log_prob = eval_KDE_matrix(train_set, test_set, best_sigma)
-        print(f'with a random seed of {r} my matrix mean log prob on mnist is {mean_log_prob}')
+        start = time.time()
+        mean_log_prob = eval_KDE_matrix(train_set, test_set, best_sigma, config['batch_size'])
+        end = time.time()
+        print(f'with a random seed of {r} my matrix mean log prob on mnist is {mean_log_prob} and it took {end-start}s')
 
 
         config['dset_name'] = "cifar"
         # load the mnist data
         train_set, valid_set, test_set = load_cifar(config)
-        test_set = test_set[:3000]
+        test_set = test_set[:2000]
 
+        start = time.time()
         KDE_sklearn = KernelDensity(bandwidth=1)
         KDE_sklearn.fit(train_set)
         log_sklearn = KDE_sklearn.score(test_set)
-        print(f'with a random seed of {r} the sklearn mean log prob on cifar is {log_sklearn/test_set.shape[0]}')
+        end = time.time()
+        print(f'with a random seed of {r} the sklearn mean log prob on cifar is {log_sklearn/test_set.shape[0]} and it took {end-start}s')
         # print('sklearn mean log prob -2868.813658031753')
 
         best_sigma = 1
+        start = time.time()
         mean_log_prob = eval_KDE(train_set, test_set, best_sigma)
-        print(f'with a random seed of {r} my mean log prob on cifar is {mean_log_prob}') 
+        end = time.time()
+        print(f'with a random seed of {r} my mean log prob on cifar is {mean_log_prob} and it took {end-start}s') 
 
-        mean_log_prob = eval_KDE_matrix(train_set, test_set, best_sigma)
-        print(f'with a random seed of {r} my matrix mean log prob on cifar is {mean_log_prob}')
+        start = time.time()
+        mean_log_prob = eval_KDE_matrix(train_set, test_set, best_sigma, config['batch_size'])
+        end = time.time()
+        print(f'with a random seed of {r} my matrix mean log prob on cifar is {mean_log_prob} and it took {end-start}s')
 
 
 if __name__ == "__main__":
